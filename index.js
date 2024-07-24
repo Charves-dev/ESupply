d955e43f96644822cee75fdae5de80d0ce10e99f
charves <charves3699@gmail.com>
2024-07-24 17:08:48 +0900

    DB 최초 접속_이후 소스반영

--- index.js
+++ index.js
@@ -1,14 +1,60 @@
 const express 	= require('express');
+const cors 		= require('cors');
+const pool 		= require('./config/db');
 const path 		= require('path');
 const app 		= express();
 const port 		= 1092;
 
+app.use(cors());
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
-app.listen(port, () => {
-	console.log('서버가 실행됩니다. http://localhost:${port}');
+
+app.post('/login', (req, res) => {
+	//console.log(req.body);
+	const { userName, userId } = req.body;
+	console.log(`UserName: ${userName}, UserId: ${userId}`);
+    res.send(`Received login data for userName: ${userName}, userId: ${userId}`);
 });
+
+app.post('/product/goodList', async (req, res) => {
+	const { product_nm, product_id } = req.body;
+	console.log(`product_nm = ${product_nm}`);
+	console.log(`product_id = ${product_id}`);
+	try{
+		conn = await pool.getConnection();
+    	const rows = await conn.query('select CLASS_ID '
+	+'    , PRODUCT_ID'
+	+'    , PRODUCT_NM'
+	+'    , PRICE'
+	+'    , WEIGHT'
+	+'    , SIZE_H'
+	+'    , SIZE_V'
+	+'    , SIZE_Z'
+    +' from esupply.product_master ');
+    
+    	res.json(rows);
+	}catch(err){
+		res.status(500).send(err.toString());
+	}finally{
+		if (conn) conn.release();
+	}
+});
+
+app.listen(port, () => {
+	console.log(`서버가 실행됩니다. http://localhost:${port}`);
+});
+
+
+
+
+
+// /product/neworder
+// /product/goodList
+// /product/goodDetail
+// /part/list
+// /part/detail')
