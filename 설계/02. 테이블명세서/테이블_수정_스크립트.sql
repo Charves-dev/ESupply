3f65eda33a7d3260b01062aacedfbdadc8fa0d59
charves <charves3699@gmail.com>
2024-07-25 18:48:41 +0900

    product_master테이블 image칼럼 추가

--- 설계/02. 테이블명세서/테이블_수정_스크립트.sql
+++ 설계/02. 테이블명세서/테이블_수정_스크립트.sql
@@ -1,3 +1,7 @@
 -- 2024.07.24 : ChangeTableName : product_out To order_out_good
 RENAME TABLE esupply.product_out TO esupply.order_out_good;
 ALTER TABLE esupply.order_out_good COMMENT='주문_출고_상품';
+
+
+-- 2024.07.25 : product_master 테이블 칼럼추가
+ALTER TABLE esupply.product_master ADD IMAGE varchar(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '제품이미지';
