e0436e8e40953470533b3dc6157df712b5033e7f
charves <charves3699@gmail.com>
2024-08-06 17:50:02 +0900

    서버비정상종료시-DB 커넥션 메모리릭방지 적용

--- config/db.js
+++ config/db.js
@@ -8,4 +8,27 @@ const pool = mariadb.createPool({
   connectionLimit: 5
 });
 
+//*************************************************************************************************
+// 서버 종료 시 연결 풀을 종료하는 핸들러
+//*************************************************************************************************
+let isPoolClosed = false;
+process.on('SIGINT', async () => {
+  if (!isPoolClosed) {
+    try {
+      await pool.end();
+      isPoolClosed = true;
+      console.log('MySQL pool closed.');
+    } catch (err) {
+      // 본 소스는 안전장치일 뿐이므로 에러날 경우 로그를 찍지않아도 된다.
+      //console.error('Error closing MySQL pool:', err.stack);
+    } finally {
+      process.exit();
+    }
+  } else {
+    console.log('MySQL pool is already closed.');
+    process.exit();
+  }
+});
+//*************************************************************************************************
+
 module.exports = pool;
