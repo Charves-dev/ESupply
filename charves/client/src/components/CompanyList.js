3e178af6aba6153cef37e0f5d2b06cfb808188b2
charves <charves3699@gmail.com>
2024-08-10 19:58:50 +0900

    (주)차베스 쿼리작업

---
+++ charves/client/src/components/CompanyList.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const CompanyList = ({ userData }) => {
+  return(
+    <div>
+      <h2>고객사목록 화면</h2>
+    </div>
+  );
+};
+
+export default CompanyList;
