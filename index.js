0d356ca3faede937f0a925da483824fabed496bd
CHOI SUN AH <csa2676@naver.com>
2024-10-05 11:14:29 +0900

    feat: esupply 이미지 정적 파일 제공 경로 추가

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'client/build')));
+app.use('/assets/Img', express.static(path.join(__dirname, 'client/public/assets/Img')));
 
 
 //*********** */
